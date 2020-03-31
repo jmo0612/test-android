1b07049c9812d79e4a57f42422222b1dd5d326fd
jimi <jimmyhardmondow@gmail.com>
2020-03-31 17:49:39 +0800

    Working Android Framework

--- app/src/main/java/com/thowo/testandroid/MainActivity.java
+++ app/src/main/java/com/thowo/testandroid/MainActivity.java
@@ -1,40 +1,52 @@
 package com.thowo.testandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.FileUtils;
+import android.view.View;
 
 import com.thowo.jmandroidframework.JMAnFunctions;
 import com.thowo.jmandroidframework.component.JMAnActivity;
 import com.thowo.jmandroidframework.component.JMAnAsyncLoaderDefault;
+import com.thowo.jmandroidframework.component.JMAnButton;
 import com.thowo.jmandroidframework.component.JMAnUIMessenger;
 import com.thowo.jmjavaframework.JMFunctions;
+import com.thowo.jmjavaframework.db.JMConnection;
+
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class MainActivity extends JMAnActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         init();
         setContentView(R.layout.activity_main);
-
+        JMAnButton b=(JMAnButton) findViewById(R.id.btnTest);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.test();
+            }
+        });
     }
 
     private void init(){
         JMFunctions.setUIListener(new JMAnUIMessenger());
         JMFunctions.setAsyncListener(new JMAnAsyncLoaderDefault(this));
-        File languageExcelFile=JMAnFunctions.copyResourceToFileDir(R.raw.jmlanguagepack,"jmlanguagepack.xlsx",false);
+        File languageExcelFile=JMAnFunctions.copyResourceToFileDir(R.raw.jmlanguagepack,"jmlanguagepack.xls",false);
         JMAnFunctions.init(languageExcelFile);
+    }
+
+    private void test(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                JMConnection con=GitIgnoreDBConnection.mySQLConnection();
+                JMFunctions.trace(con.queryMySQL("select * from user",true).getString(1));
+            }
+        }).start();
     }
 }
